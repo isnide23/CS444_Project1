5c3ceb51d6aee17b1e1ea7d77935e15638f15419
Ian Snyder <isnide23@gmail.com>
2023-04-10 11:17:06 -0700

    remove static variable from run function

--- hellothread.c
+++ hellothread.c
@@ -7,8 +7,8 @@ void *run_func(void *arg)
 {
     char *myid = (char *)arg;
   
-    static int s = 0;
-    for (s = 0; s < 5; s++)
+    
+    for (int s = 0; s < 5; s++)
         printf("%s : %d\n", myid, s);
 
     return 0;
@@ -16,17 +16,19 @@ void *run_func(void *arg)
 
 int main(void)
 {
-    char *message1 = "Thread 1";
-    char *message2 = "Thread 2";
+    char *thread_name_1 = "Thread 1";
+    char *thread_name_2 = "Thread 2";
 
     printf("Launch Threads!\n");
     pthread_t thread_1, thread_2;
 
-    pthread_create(&thread_1, NULL, run_func, (void *) message1);
-    pthread_create(&thread_2, NULL, run_func, (void *) message2);
+    pthread_create(&thread_1, NULL, run_func, (void *) thread_name_1);
+    pthread_create(&thread_2, NULL, run_func, (void *) thread_name_2);
 
     pthread_join(thread_1, NULL);
     pthread_join(thread_2, NULL);
+
+    printf("Threads Complete!\n");
     return 0;
 }
 
